8fe76832347c63c19976c743668a5c9c3e191b00
Royhan <royhankeren12@gmail.com>
2023-08-23 20:56:24 +0700

    pertama

--- lib/ui/pages/onboarding.dart
+++ lib/ui/pages/onboarding.dart
@@ -32,8 +32,77 @@ class OnBoardingPage extends StatelessWidget {
                   height: 331,
                   viewportFraction: 1,
                   enableInfiniteScroll: false,
-                  
-                ))
+                )),
+            const SizedBox(
+              height: 80,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 24),
+              padding: const EdgeInsets.symmetric(horizontal: 22, vertical: 24),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(20)),
+              child: Column(
+                children: [
+                  Text(
+                    "Grown Your\nFinancial Today",
+                    textAlign: TextAlign.center,
+                    style:
+                        blackStyle.copyWith(fontSize: 20, fontWeight: semiBold),
+                  ),
+                  const SizedBox(
+                    height: 26,
+                  ),
+                  Text(
+                    "Our system is helping you to\nachieve a better goal",
+                    style: greyStyle.copyWith(
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 12,
+                        width: 12,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: blueColor),
+                        margin: const EdgeInsets.only(right: 10),
+                      ),
+                      Container(
+                        height: 12,
+                        width: 12,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: greyColor),
+                        margin: const EdgeInsets.only(right: 10),
+                      ),
+                      Container(
+                        height: 12,
+                        width: 12,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: greyColor),
+                        margin: const EdgeInsets.only(right: 10),
+                      ),
+                      const Spacer(),
+                      SizedBox(
+                        width: 150,
+                        height: 50,
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30))),
+                            onPressed: () {},
+                            child: Text('Continue',
+                                style: whiteStyle.copyWith(
+                                    fontSize: 16, fontWeight: semiBold))),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
