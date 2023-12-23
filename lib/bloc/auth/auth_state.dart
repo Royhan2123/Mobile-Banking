16282cca7d6dae67bc329a51f8db3ece7190216e
Royhan <royhankeren12@gmail.com>
2023-12-23 15:40:02 +0700

    auth bloc

--- lib/bloc/auth/auth_state.dart
+++ lib/bloc/auth/auth_state.dart
@@ -2,9 +2,24 @@ part of 'auth_bloc.dart';
 
 sealed class AuthState extends Equatable {
   const AuthState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 final class AuthInitial extends AuthState {}
+
+final class AuthLoading extends AuthState {}
+
+final class AuthSucces extends AuthState {}
+
+
+final class AuthFailed extends AuthState {
+  final String error;
+
+  const AuthFailed(this.error);
+  @override
+  List<Object> get props => [error];
+}
+
+final class AuthCheckEmailSucces extends AuthState {}
