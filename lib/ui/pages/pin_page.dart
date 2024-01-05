ae9cd282ba1f2661477577843bf47b73cf1358d7
Royhan <royhankeren12@gmail.com>
2024-01-05 15:42:34 +0700

    pin -page

--- lib/ui/pages/pin_page.dart
+++ lib/ui/pages/pin_page.dart
@@ -20,10 +20,10 @@ class _PinPageState extends State<PinPage> {
   void initState() {
     super.initState();
 
-    final authState = context.read<AuthBloc>().state;
+    final auth = context.read<AuthBloc>().state;
 
-    if (authState is AuthSucces) {
-      pin = authState.user.pin!;
+    if (auth is AuthSucces) {
+      pin = auth.user.pin!;
     }
   }
 
@@ -64,134 +64,135 @@ class _PinPageState extends State<PinPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      backgroundColor: darkBackgroundColor,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 50),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Sha PIN",
-                style: whiteStyle.copyWith(fontSize: 20, fontWeight: bold),
-              ),
-              const SizedBox(
-                height: 75,
-              ),
-              SizedBox(
-                width: 170,
-                child: TextFormField(
-                  controller: pinController,
-                  style: whiteStyle.copyWith(
-                      fontSize: 25,
-                      fontWeight: medium,
-                      letterSpacing: 15,
-                      color: isError ? redColor : whiteColor),
-                  cursorColor: greyColor,
-                  keyboardType: TextInputType.number,
-                  obscureText: true,
-                  obscuringCharacter: "*",
-                  enabled: false,
-                  decoration: InputDecoration(
-                    disabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: greyColor)),
-                  ),
+      child: Scaffold(
+        backgroundColor: darkBackgroundColor,
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Sha PIN",
+                  style: whiteStyle.copyWith(fontSize: 20, fontWeight: bold),
                 ),
-              ),
-              const SizedBox(
-                height: 70,
-              ),
-              Wrap(
-                spacing: 40,
-                runSpacing: 40,
-                children: [
-                  CustomInputButton(
-                    title: '1',
-                    onTap: () {
-                      addPin('1');
-                    },
-                  ),
-                  CustomInputButton(
-                    title: '2',
-                    onTap: () {
-                      addPin('2');
-                    },
-                  ),
-                  CustomInputButton(
-                    title: '3',
-                    onTap: () {
-                      addPin('3');
-                    },
-                  ),
-                  CustomInputButton(
-                    title: '4',
-                    onTap: () {
-                      addPin('4');
-                    },
-                  ),
-                  CustomInputButton(
-                    title: '5',
-                    onTap: () {
-                      addPin('5');
-                    },
-                  ),
-                  CustomInputButton(
-                    title: '6',
-                    onTap: () {
-                      addPin('6');
-                    },
-                  ),
-                  CustomInputButton(
-                    title: '7',
-                    onTap: () {
-                      addPin('7');
-                    },
-                  ),
-                  CustomInputButton(
-                    title: '8',
-                    onTap: () {
-                      addPin('8');
-                    },
-                  ),
-                  CustomInputButton(
-                    title: '9',
-                    onTap: () {
-                      addPin('9');
-                    },
-                  ),
-                  const SizedBox(
-                    height: 60,
-                    width: 60,
-                  ),
-                  CustomInputButton(
-                    title: '0',
-                    onTap: () {
-                      addPin('0');
-                    },
-                  ),
-                  InkWell(
-                    onTap: () {
-                      deletePin();
-                    },
-                    child: Container(
-                      width: 60,
-                      height: 60,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: numberBackgroundColor,
-                      ),
-                      child: Center(
-                          child: Icon(Icons.arrow_back,
-                              color: whiteColor, size: 20)),
+                const SizedBox(
+                  height: 75,
+                ),
+                SizedBox(
+                  width: 170,
+                  child: TextFormField(
+                    controller: pinController,
+                    style: whiteStyle.copyWith(
+                        fontSize: 25,
+                        fontWeight: medium,
+                        letterSpacing: 15,
+                        color: isError ? Colors.red : Colors.white),
+                    cursorColor: greyColor,
+                    keyboardType: TextInputType.number,
+                    obscureText: true,
+                    obscuringCharacter: "*",
+                    enabled: false,
+                    decoration: InputDecoration(
+                      disabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: greyColor)),
                     ),
                   ),
-                ],
-              ),
-            ],
+                ),
+                const SizedBox(
+                  height: 70,
+                ),
+                Wrap(
+                  spacing: 40,
+                  runSpacing: 40,
+                  children: [
+                    CustomInputButton(
+                      title: '1',
+                      onTap: () {
+                        addPin('1');
+                      },
+                    ),
+                    CustomInputButton(
+                      title: '2',
+                      onTap: () {
+                        addPin('2');
+                      },
+                    ),
+                    CustomInputButton(
+                      title: '3',
+                      onTap: () {
+                        addPin('3');
+                      },
+                    ),
+                    CustomInputButton(
+                      title: '4',
+                      onTap: () {
+                        addPin('4');
+                      },
+                    ),
+                    CustomInputButton(
+                      title: '5',
+                      onTap: () {
+                        addPin('5');
+                      },
+                    ),
+                    CustomInputButton(
+                      title: '6',
+                      onTap: () {
+                        addPin('6');
+                      },
+                    ),
+                    CustomInputButton(
+                      title: '7',
+                      onTap: () {
+                        addPin('7');
+                      },
+                    ),
+                    CustomInputButton(
+                      title: '8',
+                      onTap: () {
+                        addPin('8');
+                      },
+                    ),
+                    CustomInputButton(
+                      title: '9',
+                      onTap: () {
+                        addPin('9');
+                      },
+                    ),
+                    const SizedBox(
+                      height: 60,
+                      width: 60,
+                    ),
+                    CustomInputButton(
+                      title: '0',
+                      onTap: () {
+                        addPin('0');
+                      },
+                    ),
+                    InkWell(
+                      onTap: () {
+                        deletePin();
+                      },
+                      child: Container(
+                        width: 60,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: numberBackgroundColor,
+                        ),
+                        child: Center(
+                            child: Icon(Icons.arrow_back,
+                                color: whiteColor, size: 20)),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
-    ));
+    );
   }
 }
