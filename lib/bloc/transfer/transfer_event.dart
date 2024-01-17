70eca0581011e6bebfede2820b4b9d911c574c80
Royhan <royhankeren12@gmail.com>
2024-01-17 10:01:26 +0700

    transfer bloc

---
+++ lib/bloc/transfer/transfer_event.dart
@@ -0,0 +1,8 @@
+part of 'transfer_bloc.dart';
+
+sealed class TransferEvent extends Equatable {
+  const TransferEvent();
+
+  @override
+  List<Object> get props => [];
+}
--- lib/services/transaction_services.dart
+++ lib/services/transaction_services.dart
@@ -4,12 +4,13 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:mobile_ebanking/models/topup_form_model.dart';
+import 'package:mobile_ebanking/models/transfer_form_model.dart';
 import 'package:mobile_ebanking/services/auth_services.dart';
 
 class TransactionServices {
   final String baseUrl = "https://bwabank.my.id/api";
 
- Future<String> topUp(TopupFormModel data) async {
+  Future<String> topUp(TopupFormModel data) async {
     try {
       print(data.toJson());
       final token = await AuthServices().getToken();
@@ -17,7 +18,7 @@ class TransactionServices {
       final res = await http.post(
         Uri.parse('$baseUrl/top_ups'),
         headers: {
-        'Authorization': 'Bearer $token',
+          'Authorization': 'Bearer $token',
         },
         body: data.toJson(),
       );
@@ -29,6 +30,29 @@ class TransactionServices {
       }
 
       throw jsonDecode(res.body)['message'];
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> trasnfer(TransferFormModel data) async {
+    try {
+      print(data.toJson());
+      final token = await AuthServices().getToken();
+
+      final res = await http.post(
+        Uri.parse('$baseUrl/transfers'),
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+        body: data.toJson(),
+      );
+
+      print(res.body);
+
+      if (res.statusCode != 200) {
+        throw jsonDecode(res.body)['message'];
+      }
     } catch (e) {
       rethrow;
     }
