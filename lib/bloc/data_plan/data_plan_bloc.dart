ad07b5be9be669fd059dd998d4399b0a3547b035
Royhan <royhankeren12@gmail.com>
2024-01-19 21:10:51 +0700

    data plan form  bloc

--- lib/bloc/data_plan/data_plan_bloc.dart
+++ lib/bloc/data_plan/data_plan_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:mobile_ebanking/models/data_plan_form_model.dart';
+import 'package:mobile_ebanking/services/transaction_services.dart';
 
 part 'data_plan_event.dart';
 part 'data_plan_state.dart';
@@ -8,7 +9,23 @@ part 'data_plan_state.dart';
 class DataPlanBloc extends Bloc<DataPlanEvent, DataPlanState> {
   DataPlanBloc() : super(DataPlanInitial()) {
     on<DataPlanEvent>((event, emit) async {
-        
+      if (event is DataPlanPost) {
+        try {
+          emit(
+            DataPlanLoading(),
+          );
+          await TransactionServices().dataPlan(event.data);
+          emit(
+            DataPlanSucces(),
+          );
+        } catch (e) {
+          emit(
+            DataPlanFailed(
+              e.toString(),
+            ),
+          );
+        }
+      }
     });
   }
 }
