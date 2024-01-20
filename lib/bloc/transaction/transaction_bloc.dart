37a5df6353e6f98c65c810c8998138b09ff73a63
Royhan <royhankeren12@gmail.com>
2024-01-20 19:38:02 +0700

    transaction bloc

--- lib/bloc/transaction/transaction_bloc.dart
+++ lib/bloc/transaction/transaction_bloc.dart
@@ -1,14 +1,32 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:mobile_ebanking/models/transaction_models.dart';
+import 'package:mobile_ebanking/services/transaction_services.dart';
 
 part 'transaction_event.dart';
 part 'transaction_state.dart';
 
 class TransactionBloc extends Bloc<TransactionEvent, TransactionState> {
   TransactionBloc() : super(TransactionInitial()) {
-    on<TransactionEvent>((event, emit) {
-      // TODO: implement event handler
+    on<TransactionEvent>((event, emit) async {
+      if (event is TransactionGet) {
+        try {
+          emit(
+            TransactionLoading(),
+          );
+          final transactionModels =
+              await TransactionServices().getTransaction();
+          emit(
+            TransactionSucces(transactionModels),
+          );
+        } catch (e) {
+          emit(
+            TransactionFailed(
+              e.toString(),
+            ),
+          );
+        }
+      }
     });
   }
 }
